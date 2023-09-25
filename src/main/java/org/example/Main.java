d897fb988670faec6ce33adb75b1a25684b86f93
mac <georghome1@gmail.com>
2023-09-25 22:55:54 +0300

    условие задачи

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,10 @@ package org.example;
 
 import java.util.Scanner;
 
-
+/*
+Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку.
+Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
+ */
 
 public class Main {
     public static void main(String[] args) {
